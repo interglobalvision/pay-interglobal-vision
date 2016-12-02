33b39d2bfa3e58d20791490aafa603f9d74deeb0
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-02 16:58:45 -0600

    set publishable key directly

--- js/main.js
+++ js/main.js
@@ -34,10 +34,10 @@ Site.Stripe = {
     var _this = this;
 
     // Stripe public keys
-    var pk_test = 'pk_test_QJv0NVjinlteY6ji0HOrah9n',
-      pk_live = 'pk_live_1iLay9wxJyeywHOFX4Q9kMtl';
+    // Text: 'pk_test_QJv0NVjinlteY6ji0HOrah9n'
+    // Live: 'pk_live_1iLay9wxJyeywHOFX4Q9kMtl'
 
-    Stripe.setPublishableKey(pk_live);
+    Stripe.setPublishableKey('pk_live_1iLay9wxJyeywHOFX4Q9kMtl');
 
     _this.createToken();
   },
