1f37d134f3c80539ff7adf2c8bd7cd7d86f96631
Patrick Best <patrickbest@patrickbest.com>
2016-12-01 18:49:11 -0600

    clean javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
-/* global jQuery, $, document, Site, Modernizr */
+/* global jQuery, $, document, Site, Stripe */
 
 Site = {
   mobileThreshold: 601,
@@ -9,14 +9,13 @@ Site = {
     _this.Stripe.init();
 
     $(window).resize(function(){
-      _this.onResize();
+//       _this.onResize();
     });
 
   },
 
   onResize: function() {
-    var _this = this;
-
+//     var _this = this;
   },
 
   fixWidows: function() {
@@ -39,7 +38,7 @@ Site.Stripe = {
     // Test:
     // pk_test_QJv0NVjinlteY6ji0HOrah9n
     //
-    // Live: 
+    // Live:
     // pk_live_1iLay9wxJyeywHOFX4Q9kMtl
 
     Stripe.setPublishableKey('pk_test_QJv0NVjinlteY6ji0HOrah9n');
@@ -50,11 +49,11 @@ Site.Stripe = {
   createToken: function() {
     var _this = this;
 
-    _this.$form.submit(function(event) {
+    _this.$form.submit(function() {
 
       // Clear response
       $('#payment-response').removeClass('show approved declined').html('&nbsp;');
-      
+
       // Disable the submit button to prevent repeated clicks:
       _this.$form.find('.submit').prop('disabled', true);
 
@@ -90,30 +89,32 @@ Site.Stripe = {
         method: "POST",
         data: {form: values}
       });
-       
+
       request.done(function( msg ) {
         _this.$form.find('input[type=text], textarea').val(''); // Clear form values
         _this.$form.find('.submit').prop('disabled', false); // Re-enable submission
         $('#stripeToken').remove(); // Remove token input
 
-        var responseClass,
-          responseMsg;
+        var responseClass;
+        var responseMessage;
 
-        console.log(msg);
+        if (msg === 'authorized') {
 
-        if (msg == 'authorized') {
           responseClass = 'authorized';
           responseMessage = 'Your payment has been authorized. Thank you.';
-        } else if (msg == 'manual_review') {
+
+        } else if (msg === 'manual_review') {
+
           responseClass = 'authorized';
           responseMessage = 'Your payment is in review. We will contact you at the provided email if any further action is required. Thank you.';
+
         } else {
           // Stripe error caught
           try {
             // try to parse json. this would indicated a '\Stripe\Error\Card' was returned.
             var parsedData = JSON.parse(msg);
+
             responseClass = 'declined';
-            console.log(parsedData);
             responseMessage = parsedData.message;
           } catch (e) {
             // not json data! this indicates a different error. so just print the error message defined in charge.php
@@ -124,7 +125,7 @@ Site.Stripe = {
 
         $('#payment-response').addClass('show ' + responseClass).html(responseMessage);
       });
-       
+
       request.fail(function( jqXHR, textStatus ) {
         _this.$form.find('.submit').prop('disabled', false);
         $('#payment-response').addClass('show declined').html(textStatus + '. Please try again.');
